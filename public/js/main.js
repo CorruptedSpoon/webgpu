e442cf53068627acfb7c1607a8d20bbbef24e84a
CorruptedSpoon <vanedelman@gmail.com>
2024-05-27 17:30:16 -0400

    basic color interpolation with inter-stage variable

--- public/js/main.js
+++ public/js/main.js
@@ -11,21 +11,35 @@ const main  = async () => {
     context.configure({device, format: presentationFormat});
 
     const shaderModule = device.createShaderModule({
-        label: 'basic red triangle',
-        code: `
+        label: 'basic rgb triangle',
+        code: /* wgsl */`
+            struct VertexShaderOutput {
+                @builtin(position) position : vec4f,
+                @location(0) color: vec4f
+            }
+
             @vertex fn vs(
                 @builtin(vertex_index) vertexIndex : u32
-            ) -> @builtin(position) vec4f {
+            ) -> VertexShaderOutput {
                 let pos = array(
                     vec2f(0.0, 0.5), // top center
                     vec2f(-0.5, -0.5), // bottom left
                     vec2f(0.5, -0.5) // bottom right
                 );
-                return vec4f(pos[vertexIndex], 0.0, 1.0);
+                var color = array<vec4f, 3>(
+                    vec4f(1.0, 1.0, 0.0, 1.0),
+                    vec4f(0.0, 1.0, 1.0, 1.0),
+                    vec4f(1.0, 0.0, 1.0, 1.0)
+                );
+
+                var vsOutput: VertexShaderOutput;
+                vsOutput.position = vec4f(pos[vertexIndex], 0.0, 1.0);
+                vsOutput.color = color[vertexIndex];
+                return vsOutput;
             }
 
-            @fragment fn fs() -> @location(0) vec4f {
-                return vec4f(1.0, 0.0, 0.0, 1.0);
+            @fragment fn fs(fsInput: VertexShaderOutput) -> @location(0) vec4f {
+                return fsInput.color;
             }
         `
     });
@@ -49,7 +63,7 @@ const main  = async () => {
         colorAttachments: [
             {
                 // view: <- to be filled in by the render loop
-                clearValue: [0.0, 0.0, 1.0, 1],
+                clearValue: [0.0, 0.0, 0.0, 1],
                 loadOp: 'clear',
                 storeOp: 'store'
             }
